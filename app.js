e9f49d895160d0bef38a9dd399e55ce3f81c8067
scOnTz <piyapat_sam_@hotmail.com>
2017-03-05 04:12:54 +0700

    Update My Page

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 /* 
 var http = require('http');
 http.createServer(function (req, res) {
@@ -12,28 +11,71 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static('public'));
+app.use('/bower_components', express.static('bower_components'));
 var mustacheExpress = require('mustache-express');
-app.engine('mustache',mustacheExpress());
+app.engine('mustache', mustacheExpress());
 
-app.set('view engine','mustache');
-app.set('views',__dirname + '/views');
+app.set('view engine', 'mustache');
+app.set('views', __dirname + '/views');
 
 app.get('/', function (req, res) {
   res.send('Hello NodeJS');
 })
 
-function getImagePath(){
+function getImagePath() {
   return 'http://wallpaper-gallery.net/images/image/image-13.jpg';
 }
-app.post('/horoscope', function (req, res){
-  
-  res.render('horoscope',{text:req.body.pass});
-})
 
-app.get('/mypage', function (req, res){
-  res.render('mypage',{imgPath: getImagePath()});
+function getPostIt() {
+  var data = [];
+  var isColor = ['is-primary','is-info','is-success','is-warning','is-danger'];
+  var isColorIdx = 0;
+  for (var i = 0; i < 6; i++) {
+    if(isColorIdx > 4){
+      isColorIdx = 0;
+    }
+    data.push({
+      actName: 'First NodeJS Day ' + (i + 1),
+      actContent: 'เขียน NodeJS ครั้งแรก',
+      actDate: formatDate(d),
+      actTime: d.toLocaleTimeString(),
+      actColor: '<p class="notification '+ isColor[isColorIdx] +' has-text-centered">'
+    });
+    isColorIdx++;
+  }
+  return data;
+}
+
+function formatDate(date) {
+  var monthNames = [
+    "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+  ];
+
+  var day = date.getDate();
+  var monthIndex = date.getMonth();
+  var year = date.getFullYear();
+
+  return day + ' ' + monthNames[monthIndex] + ' ' + year;
+}
+app.post('/horoscope', function (req, res) {
+
+  res.render('horoscope', {
+    text: req.body.pass
+  });
+})
+var d = new Date();
+
+
+var actArr = getPostIt();
+console.log(actArr);
+app.get('/mypage', function (req, res) {
+  res.render('mypage', {
+    items: actArr
+  });
 })
 app.listen(3000);
 console.log('Server Running...');
